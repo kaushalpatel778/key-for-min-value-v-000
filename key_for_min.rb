9e2423c1e6e1d014834f6fa804e866b2b5cbefb8
Kaushal Patel <kaushalpatel778@gmail.com>
2018-03-23 03:20:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -30,22 +30,5 @@ def key_for_min_value(name_hash)
     return  name_hash.collect.min_by {|keys,values| values}
 end
 =end
-=======
 
 
-  if name_hash = {}
-    return nil
-  else
-      min_value = name_hash.first[1]
-      min_key = name_hash.first[0]
-
-      name_hash.collect do |key, value|
-        if value < min_value
-          min_value = value
-          min_key = key
-        end
-      end
-  end
-return name_hash
-end
->>>>>>> 10b339adb882342a0c49377fb9af986cfdd6ab2b
